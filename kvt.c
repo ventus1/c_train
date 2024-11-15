ee92ec29d277a4dcf8776b316c9eda9740424bf3
ventus1 <khab2014@mail.ru>
2024-11-15 16:31:11 +0300

    deleting

--- kvt.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    double a,b,c;
-    scanf("%lf %lf %lf", &a,&b,&c);
-    //printf("%ld %ld %ld", a,b,c);
-    printf("%lf", (b-a) * c);
-}
--- longdouble.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  double db = 5.321;
-  printf("%7.2lf,%d",db);
-  return 0;
-}
-// pwduser:Ks9
