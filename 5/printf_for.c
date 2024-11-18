bf1169b72744544954f94f39b6e5ac36f57943c0
ventus1 <khab2014@mail.ru>
2024-11-18 17:12:40 +0300

    Fifth day of C

--- 5/printf_for.c
+++ 5/printf_for.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a,b,diff,size;
+    int a,b,size;
     size = 0;
     scanf("%d %d", &a,&b);
-    diff = a+b;
     for (int i = a; i <= b; i++){
-        
         for (int k = 0; k <= size; k++){
             printf("%4d ", i);
         }
